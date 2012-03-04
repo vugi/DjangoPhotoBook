faef0f896659ae5ee96979a85ea5f82f62409fc0
Toni Maki <maki.toni@gmail.com>
2012-03-04 17:59:23 +0200

    Added ability to remove images, but is bugged at the moment

--- django_photobook/photobook/static/js/edit.js
+++ django_photobook/photobook/static/js/edit.js
@@ -20,7 +20,7 @@ function makeEditable(){
 }
 
 $(function() {
-	
+	var $currentPicture = undefined;
 	loadPage(album,page,function(){
 		makeEditable();
 	});
@@ -29,7 +29,8 @@ $(function() {
 		var url = $("#newImageUrl").val();
 		console.log(url);
 		var $img = $("<img>")
-			.attr("src",url)
+			.attr({src: url,
+                   id: 'addedImage'})
 			.appendTo("#page");
 
 		makeEditable();
@@ -39,11 +40,23 @@ $(function() {
         var url = $(this).attr("src");
 		console.log(url);
 		var $img = $("<img>")
-			.attr("src",url)
+			.attr({src: url,
+                   id: 'addedImage'})
 			.appendTo("#page");
 
 		makeEditable();
-    });  
+    });
+    
+    $(document).on("click", "#addedImage", function(){
+        console.log("Clicked a addedImage.");
+        $currentPicture = $(this)
+    });
+    
+    $("#deleteImage").click(function() {
+        if ($currentPicture!== undefined) {
+            $currentPicture.remove();
+        }
+    });
 	
 	$("#newImageUrl").bind("propertychange keyup input paste", function(){
 		console.log("changed");
